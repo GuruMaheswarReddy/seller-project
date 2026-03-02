6447915b710dacf8381573adb3a2ca66b3c5524f
GuruMaheswarReddy <gurumahesh478@gmail.com>
2026-03-02 11:07:34 +0530

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header.jsx'
+import Footer from "../components/Footer.jsx"
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen text-gray-900">
+    <div className="min-h-screen text-gray-900 flex flex-col">
       <Header />
-      
-        
-          <main className="w-full">
+
+      <main className="w-full flex-grow">
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   )
 }
 
 export default MainLayout
-
-
